9c80659ecbc5081ec6dfb2b62d730a11e54971a5
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2024-12-16 01:21:57 +0600

    first-commit

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class AdminDashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard.index');
+        // return view('admin.dashboard.index');
+        return view('admin.layouts.master');
     }
 }
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
+    @stack('admin-css')
+</head>
+
+<body>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            <!-- include  navbar-->
+            @include('admin.layouts.navbar')
+            <!-- include  sidebar-->
+            @include('admin.layouts.sidebar')
+
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield('admin-content')
+            </div>
+            <footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a href="javascript:;">MD.
+                        Faysal Hossain Tibro</a>
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
+        </div>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
+    <!-- Template JS File -->
+    <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
+    @stack('admin-js')
+</body>
+
+</html>
