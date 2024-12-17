e45a9953d52697f438822b42af7788a381fe2e02
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2024-12-17 15:31:53 +0600

    slider

--- resources/views/frontend/home/component/carousel.blade.php
+++ resources/views/frontend/home/component/carousel.blade.php
@@ -1,68 +1,23 @@
 <div class="container-fluid p-0 pb-5">
     <div class="owl-carousel header-carousel position-relative">
-        <div class="owl-carousel-item position-relative">
-            <img class="img-fluid" src="{{ asset('frontend/img/carousel-1.jpg') }}" alt="">
-            <div class="carousel-inner">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-12 col-lg-8 text-center">
-                            <h1 class="display-3 text-white animated slideInDown mb-4">Best CCTV & Security
-                                Solution For You</h1>
-                            <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed
-                                stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                            <a href=""
-                                class="btn btn-primary rounded-pill py-md-3 px-md-5 me-3 animated slideInLeft">Read
-                                More</a>
-                            <a href=""
-                                class="btn btn-light rounded-pill py-md-3 px-md-5 animated slideInRight">Free
-                                Quote</a>
+        @foreach ($sliders as $slider)
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" src="{{ asset($slider->image) }}">
+                <div class="carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-lg-8 text-center">
+                                <h1 class="display-3 text-white animated slideInDown mb-4">{{ $slider->title }}</h1>
+                                <p class="fs-5 text-white mb-4 pb-2">{{ $slider->description }}</p>
+                                <a href="{{ $slider->first_button_url }}"
+                                    class="btn btn-primary rounded-pill py-md-3 px-md-5 me-3 animated slideInLeft">{{ $slider->first_button_name }}</a>
+                                <a href="{{ $slider->second_button_url }}"
+                                    class="btn btn-light rounded-pill py-md-3 px-md-5 animated slideInRight">{{ $slider->second_button_name }}</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="owl-carousel-item position-relative">
-            <img class="img-fluid" src="{{ asset('frontend/img/carousel-2.jpg') }}" alt="">
-            <div class="carousel-inner">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-12 col-lg-8 text-center">
-                            <h1 class="display-3 text-white animated slideInDown mb-4">Smart Security Solution For
-                                All Business</h1>
-                            <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed
-                                stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                            <a href=""
-                                class="btn btn-primary rounded-pill py-md-3 px-md-5 me-3 animated slideInLeft">Read
-                                More</a>
-                            <a href=""
-                                class="btn btn-light rounded-pill py-md-3 px-md-5 animated slideInRight">Free
-                                Quote</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="owl-carousel-item position-relative">
-            <img class="img-fluid" src="{{ asset('frontend/img/carousel-3.jpg') }}" alt="">
-            <div class="carousel-inner">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-12 col-lg-8 text-center">
-                            <h1 class="display-3 text-white animated slideInDown mb-4">Innovative Solution For
-                                Security System</h1>
-                            <p class="fs-5 fw-medium text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum
-                                dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum
-                                sea elitr.</p>
-                            <a href=""
-                                class="btn btn-primary rounded-pill py-md-3 px-md-5 me-3 animated slideInLeft">Read
-                                More</a>
-                            <a href=""
-                                class="btn btn-light rounded-pill py-md-3 px-md-5 animated slideInRight">Free
-                                Quote</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </div>
